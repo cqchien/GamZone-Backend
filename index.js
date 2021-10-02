d1080c9d337f1bde92714f8524af9060fcd48aa2
HungDangQuang <hungdangkg@gmail.com>
2021-10-02 17:08:18 +0700

    config server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const express = require("express")
+var app = express()
+
+app.get("/", function(res,res){
+    res.json("Home")
+})
+
+app.listen(3000, function(){
+    console.log("Server start on port ")
+})
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.urlencoded({extended:false}))
+app.use(express.json({}));
