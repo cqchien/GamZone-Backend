05bced6612a0dd4dc65ca8b17c899b62c4673c8c
HungDangQuang <hungdangkg@gmail.com>
2021-10-25 17:40:29 +0700

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,1 +1,28 @@
 const httpStatus = require('http-status')
+const bcrypt = require('bcryptjs')
+const getUserByEmailOrId = require('../services/user/getOne.user')
+
+const register = async (req, res, next) => {
+    const { email, password, name } = req.body;
+    try {
+      // Check Email
+      const user = await getUserByEmailOrId({ email });
+      if (user) {
+        throw new Exception(httpStatus.CONFLICT, 'Email Already Taken');
+      }
+  
+      // Create new User
+      const newUser = await createUser({
+        email,
+        name,
+        password,
+      });
+  
+      // Create Token
+      const token = await generateAuthToken(newUser);
+  
+      return handleSuccess(res, { token }, httpStatus.CREATED);
+    } catch (error) {
+      next(error);
+    }
+  };
