b649d44d4c57cae3c8e8d9ec4e48b886501e1e5e
HungDangQuang <hungdangkg@gmail.com>
2021-10-25 17:10:25 +0700

    modify module registration

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -3,6 +3,12 @@ const Joi = require('joi');
 const registerValidationSchema = Joi.object().keys({
   name: Joi.string().required(),
   email: Joi.string().required().email(),
+  password: Joi.string().required()
 });
 
-module.exports = registerValidationSchema
+const loginValidationSchema = Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+});
+
+module.exports = {registerValidationSchema, loginValidationSchema}
