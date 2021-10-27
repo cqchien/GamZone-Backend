c6ced3bf6e57b5a5c43cd7eb4ab5b2fea9f92181
HungDangQuang <hungdangkg@gmail.com>
2021-10-27 16:45:09 +0700

    add route login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 const Router = require("express");
-const register = require("../controllers/user.controller");
+const {register,login} = require("../controllers/user.controller");
 const validate = require("../middlewares/validate.middleware");
-const { registerValidationSchema } = require("../validation/auth.validation");
+const { registerValidationSchema,loginValidationSchema } = require("../validation/auth.validation");
 
 const router = Router();
 
 router.post("/register", validate(registerValidationSchema), register);
+router.post('/login', validate(loginValidationSchema), login);
 
 module.exports = router;
