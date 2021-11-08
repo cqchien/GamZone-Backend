a7ccfc98b99973c0f8180507156c9cc26272056a
HungDangQuang <hungdangkg@gmail.com>
2021-11-08 20:45:15 +0700

    modify generating token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -42,7 +42,7 @@ const register = async (req, res, next) => {
       }
   
       // create token
-      const token = await   (user);
+      const token = await generateAuthToken(user);
       
       return handleSuccess(res, { token }, httpStatus.CREATED);
     } catch (error) {
