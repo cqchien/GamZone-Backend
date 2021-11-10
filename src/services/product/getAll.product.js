7e1a0c7c356ed1299e8317413fe6188d18085135
HungDangQuang <hungdangkg@gmail.com>
2021-11-10 22:32:19 +0700

    add service find all products

---
+++ src/services/product/getAll.product.js
@@ -0,0 +1,9 @@
+const { model } = require('mongoose')
+const Product= require('../../models/product.model')
+//name, password, dob, address, phone, email, avatar
+const getALlProducts = async () => {
+    const productsList = await Product.find()   
+    return productsList
+};
+
+module.exports = getALlProducts
