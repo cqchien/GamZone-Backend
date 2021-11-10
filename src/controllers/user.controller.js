35b78e09c3653d08797f899416cb800beef5515c
cqchien <caochientp1@gmail.com>
2021-11-10 23:55:14 +0700

    update checkToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,52 +1,57 @@
-const httpStatus = require('http-status')
-const bcrypt = require('bcryptjs')
-const getUserByEmailOrId = require('../services/user/getOne.user')
-const Exception = require('../utils/exception')
-const handleSuccess = require('../utils/successfulHandler')
-const createUser = require('../services/user/create.user')
-const generateAuthToken = require('../services/token/generateAuth.Service')
+const httpStatus = require("http-status");
+const bcrypt = require("bcryptjs");
+const getUserByEmailOrId = require("../services/user/getOne.user");
+const Exception = require("../utils/exception");
+const handleSuccess = require("../utils/successfulHandler");
+const createUser = require("../services/user/create.user");
+const generateAuthToken = require("../services/token/generateAuth.Service");
 const register = async (req, res, next) => {
-    const { name, password, email } = req.body;
-    try {
-      console.log(req.body)
-      // Check Email
-      const user = await getUserByEmailOrId({ email });
-      if (user) {
-        throw new Exception(httpStatus.CONFLICT, 'Email Already Taken');
-      }
-  
-      // Create new User
-      const newUser = await createUser({
-        name, password, email
-      });
-  
+  const { name, password, email } = req.body;
+  try {
+    console.log(req.body);
+    // Check Email
+    const user = await getUserByEmailOrId({ email });
+    if (user) {
+      throw new Exception(httpStatus.CONFLICT, "Email Already Taken");
+    }
+
+    // Create new User
+    const newUser = await createUser({
+      name,
+      password,
+      email,
+    });
+
     // Create token
-    const token = await generateAuthToken(newUser)
+    const token = await generateAuthToken(newUser);
 
-    return handleSuccess(res, {token}, httpStatus.CREATED)
-    } catch (error) {
-      next(error);
-    }
-  };
+    return handleSuccess(res, { token }, httpStatus.CREATED);
+  } catch (error) {
+    next(error);
+  }
+};
 
-  const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-      // check Email
-      const user = await getUserByEmailOrId({ email });
-  
-      // check password whether match or not
-      const isMatchPassword = await bcrypt.compare(password, user.password);
-      if (!isMatchPassword || !user) {
-        throw new Exception(httpStatus.UNAUTHORIZED, 'Incorrect Email Or Password');
-      }
-  
-      // create token
-      const token = await generateAuthToken(user);
-      
-      return handleSuccess(res, { token }, httpStatus.CREATED);
-    } catch (error) {
-      next(error);
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // check Email
+    const user = await getUserByEmailOrId({ email });
+    const userPassword = user?.password ? user.password : "";
+    // check password whether match or not
+    const isMatchPassword = await bcrypt.compare(password, userPassword);
+    if (!isMatchPassword || !user) {
+      throw new Exception(
+        httpStatus.UNAUTHORIZED,
+        "Incorrect Email Or Password"
+      );
     }
-  };
-  module.exports = {register,login}
+
+    // create token
+    const token = await generateAuthToken(user);
+
+    return handleSuccess(res, { token }, httpStatus.CREATED);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { register, login };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,11 +1,24 @@
 const Router = require("express");
-const {createProduct, getAllProducts} = require("../controllers/product.controller");
+const {
+  createProduct,
+  getAllProducts,
+} = require("../controllers/product.controller");
+const checkToken = require("../middlewares/checkToken.middleware");
 const validate = require("../middlewares/validate.middleware");
-const { createProductValidationSchema } = require("../validation/product.validation");
+const {
+  createProductValidationSchema,
+} = require("../validation/product.validation");
 
 const router = Router();
 
-router.post("/createProduct", validate(createProductValidationSchema), createProduct)
-router.get("/getAllProducts",getAllProducts)
+router.get("/getAllProducts", getAllProducts);
+
+router.use(checkToken);
+
+router.post(
+  "/createProduct",
+  validate(createProductValidationSchema),
+  createProduct
+);
 
 module.exports = router;
