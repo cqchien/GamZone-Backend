fd2fe383f7d5378d26976048d746a886e6f663d0
HungDangQuang <hungdangkg@gmail.com>
2021-11-10 20:04:09 +0700

    add createProductValidationSchema

---
+++ src/validation/product.validation.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const createProductValidationSchema = Joi.object().keys({
+  name: Joi.string().required(),
+  price: Joi.string().required(),
+  shortDescription: Joi.string().required(),
+  SKU: Joi.string().required(),
+  quantity: Joi.string().required()
+});
