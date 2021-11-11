e9fe6eb83e87a9b29f04a5490f7a2d443e10cfa9
cqchien <caochientp1@gmail.com>
2021-11-11 10:51:54 +0700

    update validation and add field role in createUser

--- src/services/user/create.user.js
+++ src/services/user/create.user.js
@@ -1,23 +1,16 @@
-const bcrypt = require('bcryptjs')
-const { model } = require('mongoose')
-const User = require('../../models/user')
-//name, password, dob, address, phone, email, avatar
-const createUser = async ({
-    name, password, dob, address, phone, email, avatar
-  }) => {
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, 10);
-    const newUser = await User.create({
-        name, 
-        password: hash, 
-        dob, 
-        address, 
-        phone, 
-        email, 
-        avatar
-    });
-  
-    return newUser;
-  };
+const bcrypt = require("bcryptjs");
+const User = require("../../models/user");
 
-module.exports = createUser
+const createUser = async ({ name, password, email, role }) => {
+  const hash = bcrypt.hashSync(password, 10);
+  const newUser = await User.create({
+    name,
+    password: hash,
+    role,
+    email,
+  });
+
+  return newUser;
+};
+
+module.exports = createUser;
--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -1,14 +1,15 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const registerValidationSchema = Joi.object().keys({
   name: Joi.string().required(),
   password: Joi.string().required(),
-  email: Joi.string().required().email()
+  email: Joi.string().required().email(),
+  role: Joi.string().empty(""),
 });
 
 const loginValidationSchema = Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().required(),
 });
 
-module.exports = {registerValidationSchema, loginValidationSchema}
+module.exports = { registerValidationSchema, loginValidationSchema };
