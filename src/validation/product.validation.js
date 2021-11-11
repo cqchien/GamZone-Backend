34ba5795ad86b9f79ce18ac15f5df43d5e95f586
cqchien <caochientp1@gmail.com>
2021-11-11 17:48:34 +0700

    update validation and docs

--- src/validation/product.validation.js
+++ src/validation/product.validation.js
@@ -1,11 +1,11 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const createProductValidationSchema = Joi.object().keys({
   name: Joi.string().required(),
   price: Joi.string().required(),
   shortDescription: Joi.string().required(),
   SKU: Joi.string().required(),
-  quantity: Joi.string().required()
+  quantity: Joi.number().required(),
 });
 
-module.exports = {createProductValidationSchema}
+module.exports = { createProductValidationSchema };
