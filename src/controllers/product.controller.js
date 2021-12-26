79d76ec9cccc9de45d6164cedcd45784fff471e5
cqchien <caochientp1@gmail.com>
2021-12-26 14:48:56 +0700

    get detail product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,7 @@
 const httpStatus = require("http-status");
 const ProductModel = require("../models/product.model");
+const SpecificationModel = require("../models/specification.model");
+const Exception = require("../utils/exception");
 const handleSuccess = require("../utils/successfulHandler");
 
 // api: Lấy tất cả sản phẩm và phân trang
@@ -28,6 +30,33 @@ const getAllProducts = async (req, res, next) => {
   }
 };
 
+// api: Lấy 1 sản phẩm theo id
+const getProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Lấy tổng quan sản phẩm
+    let product = await ProductModel.findOne({ _id: id }).populate('category');
+
+    if (!product) {
+      throw new Exception(
+        httpStatus.NOT_FOUND,
+        "Product Not Found"
+      );
+    }
+    // Lấy mô tả sản phẩm
+    const productSpecifications = await SpecificationModel.find({
+      product: product._id
+    }).select('-_id -product -__v');
+
+    // Trả về
+    return handleSuccess(res, { product, specifications: productSpecifications }, httpStatus.OK);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllProducts,
+  getProduct
 };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,11 @@
 const Router = require("express");
-const { getAllProducts } = require("../controllers/product.controller");
-const checkToken = require("../middlewares/checkToken.middleware");
+const { getAllProducts, getProduct } = require("../controllers/product.controller");
 
 const router = Router();
 // api: lấy danh sách và phân trang
 router.get('/all', getAllProducts);
+
+// api: Lấy 1 sản phẩm theo id
+router.get('/:id', getProduct);
 
 module.exports = router;
