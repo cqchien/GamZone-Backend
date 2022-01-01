9cce38f74657f28e96f6a2df313fbd242b8f7b2e
cqchien <caochientp1@gmail.com>
2022-01-02 03:01:14 +0700

    update in route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 const Router = require("express");
 const { getListOrderForAdmin, updateOrderStatusForAdmin } = require("../controllers/order.controller");
-const { DoStatisticMonthlyRevenue, DoStatisticAnnualRevenue } = require("../controllers/statistic.model");
+const { doStatisticMonthlyRevenue, doStatisticAnnualRevenue } = require("../controllers/statistic.model");
 
 
 const router = Router();
@@ -9,7 +9,7 @@ const router = Router();
 router.get("/order", getListOrderForAdmin);
 router.post("/order/:id", updateOrderStatusForAdmin);
 
-router.get("/monthly-revenue", DoStatisticMonthlyRevenue);
-router.get("/annual-revenue", DoStatisticAnnualRevenue);
+router.get("/monthly-revenue", doStatisticMonthlyRevenue);
+router.get("/annual-revenue", doStatisticAnnualRevenue);
 
 module.exports = router;
